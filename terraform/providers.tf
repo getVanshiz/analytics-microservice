eeada9202666077ee68e6dbcf98f3d14cded920f
Vanshika (Temp) <studio.vanshiz@gmail.com>
2026-02-09 21:51:29 +0530

    Jenkins error

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,15 @@
 
 # Kubernetes provider
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "rancher-desktop"
+  config_path    = fileexists("~/.kube/config") ? "~/.kube/config" : ""
+  config_context = fileexists("~/.kube/config") ? "rancher-desktop" : ""
+
 }
 
 # Helm provider - ARGUMENT form to forward Kubernetes auth
 provider "helm" {
   kubernetes = {
-    config_path    = "~/.kube/config"
-    config_context = "rancher-desktop"
+    config_path    = fileexists("~/.kube/config") ? "~/.kube/config" : ""
+    config_context = fileexists("~/.kube/config") ? "rancher-desktop" : ""
   }
 }
